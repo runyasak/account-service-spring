b51873517ac3eee9e61e311b5792470b8fc625ba
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-06 15:30:10 +0700

    feat: add Account model

---
+++ src/main/java/com/example/accountservice/model/Account.java
@@ -0,0 +1,24 @@
+package com.example.accountservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String user;
+}
